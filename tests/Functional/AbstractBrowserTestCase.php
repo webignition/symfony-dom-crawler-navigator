c7b1d1381cf018c39e790a0d06d707a63051be74
Jon Cram <jon@webignition.net>
2020-02-26 19:05:39 +0000

    Use new webignition/base-panther-test-case

--- tests/Functional/AbstractBrowserTestCase.php
+++ tests/Functional/AbstractBrowserTestCase.php
@@ -4,51 +4,14 @@ declare(strict_types=1);
 
 namespace webignition\SymfonyDomCrawlerNavigator\Tests\Functional;
 
-use PHPUnit\Framework\TestCase;
-use Symfony\Component\Panther\Client;
-use Symfony\Component\Panther\Client as PantherClient;
-use webignition\SymfonyPantherWebServerRunner\Options;
-use webignition\SymfonyPantherWebServerRunner\WebServerRunner;
-
-abstract class AbstractBrowserTestCase extends TestCase
+abstract class AbstractBrowserTestCase extends \webignition\BasePantherTestCase\AbstractBrowserTestCase
 {
     private const WEB_SERVER_DIR = __DIR__ . '/../fixtures/html';
 
-    /**
-     * @var WebServerRunner
-     */
-    private static $webServerRunner;
-
-    /**
-     * @var PantherClient
-     */
-    protected static $client;
-
-    /**
-     * @var string|null
-     */
-    protected static $webServerDir;
-
-    /**
-     * @var string|null
-     */
-    protected static $baseUri;
-
     public static function setUpBeforeClass(): void
     {
-        if (null === self::$baseUri) {
-            self::$baseUri = Options::getBaseUri();
-        }
+        self::$webServerDir = self::WEB_SERVER_DIR;
 
-        self::$webServerRunner = new WebServerRunner((string) realpath(self::WEB_SERVER_DIR));
-        self::$webServerRunner->start();
-
-        self::$client = Client::createChromeClient(null, null, [], self::$baseUri);
-    }
-
-    public static function tearDownAfterClass(): void
-    {
-        self::$webServerRunner->stop();
-        self::$client->quit();
+        parent::setUpBeforeClass();
     }
 }
